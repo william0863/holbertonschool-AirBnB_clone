7bb00cf61fb5fe052e5e25f2eb6cc3fccb369118
william0863 <granger.w35@gmail.com>
2022-10-13 16:01:15 +0200

    fix basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 module "base_model"
 """
 
-from uuid import uuid4
+import uuid
 from datetime import datetime
 import models
 
@@ -30,10 +30,10 @@ class BaseModel:
                 else:
                     setattr(self, key, val)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            # models.storage.new(self)
 
     def __str__(self):
         """
@@ -47,7 +47,7 @@ class BaseModel:
         updates "updated_at" instance with current datetime
         """
         self.updated_at = datetime.now()
-        models.storage.save()
+        # models.storage.save()
 
     def to_dict(self):
         """
