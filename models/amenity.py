1ec8eb1ddf1fbbd2846387ce79748c0699bfa321
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-10-16 17:27:23 +0200

    typo

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-contains the Amenety class
+contains the Amenty class
 
 """
 
 
 from models.base_model import BaseModel
 
-class Amenety(BaseModel):
+class Amenity(BaseModel):
     """ Amenety has the following attributes:
-    name: the name of the Amenety
+    name: the name of the Amenity
     """
     name = ""
