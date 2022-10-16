a75faa51e27dc3c4158f999979364ea48d484250
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-10-16 16:36:29 +0200

    adding user subclass

---
+++ models/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+contains the user class
+
+"""
+
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ User has the following attributes:
+    email (string): the email of the user
+    password (string): the password of the user
+    first_name (string): the first name of the user
+    last_name (string): the last name of the user
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
